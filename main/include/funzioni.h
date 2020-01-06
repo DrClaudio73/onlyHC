90bb9dc98d9457e8f23699704ffccecf59578924
Claudio Cattaneo <ccattaneo73@gmail.com>
2020-01-06 09:01:58 +0100

    updated header file

--- main/include/funzioni.h
+++ main/include/funzioni.h
@@ -5,7 +5,9 @@
 
 
 unsigned char calcola_CRC(int totale);
-void aggiorna(unsigned char* msg_to_send, const unsigned char* valore, unsigned char* k, int* totale);
+void pack_str(unsigned char* msg_to_send, const unsigned char* valore, unsigned char* k, int* totale);
+void pack_num(unsigned char* msg_to_send, unsigned char valore, unsigned char* k, int* totale);
+
 unsigned char* pack_msg(unsigned char uart_controller, unsigned char addr_from, unsigned char addr_to, const unsigned char* cmd, const unsigned char* param);
 
 unsigned char unpack_msg(const unsigned char* msg, unsigned char allowed_addr_from, unsigned char allowed_addr_to, unsigned char** cmd, unsigned char** param);
