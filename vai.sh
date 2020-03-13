cf7e25177ad78834ae9dfa653b2c062c0bbab208
Claudio Cattaneo <ccattaneo73@gmail.com>
2020-03-13 08:56:46 +0100

    finalized launcher

--- vai.sh
+++ vai.sh
@@ -5,29 +5,53 @@ echo "-V[only view]: monitor"
 echo "-N[new]: build app-flash monitor"
 echo "-0=/dev/ttyUSB0 ; -1=/dev/ttyUSB1"
 echo "-M= master ; -S=Slave; -L=Mobile"
-PORTA=""
+PORTA_="-p/dev/ttyUSB0"
+
+#default is STATION_MASTER
+STATION_="#define DEVOPS_THIS_IS_STATION_MASTER"
 
 while getopts BVN01MSL option; do
 case ${option}
 in
-0)  PORTA="-p/dev/ttyUSB0";;
-1)  PORTA="-p/dev/ttyUSB1";;
-B)  BUILD1="build"; BUILD2=""; BUILD3=""; PORTA="";;
-V)  BUILD1="monitor"; BUILD2=""; BUILD3="";;
-N)  BUILD1="build"; BUILD2="app-flash"; BUILD3="monitor";;
-M)  STATION="#define DEVOPS_THIS_IS_STATION_MASTER";;
-S)  STATION="#define DEVOPS_THIS_IS_STATION_SLAVE";;
-L)  STATION="#define DEVOPS_THIS_IS_STATION_MOBILE";;
+0)  PORTA_="-p/dev/ttyUSB0";;
+1)  PORTA_="-p/dev/ttyUSB1";;
+B)  BUILD1_="build";; #BUILD2_=; BUILD3_=; PORTA_=;;
+V)  BUILD1_="monitor";; #BUILD2_=""; BUILD3_="";;
+N)  BUILD1_="build"; BUILD2_="app-flash"; BUILD3_="monitor";;
+M)  STATION_="#define DEVOPS_THIS_IS_STATION_MASTER";;
+S)  STATION_="#define DEVOPS_THIS_IS_STATION_SLAVE";;
+L)  STATION_="#define DEVOPS_THIS_IS_STATION_MOBILE";;
 esac
 done
 
+echo "BUILD1: ${BUILD1_} BUILD2: ${BUILD2_} BUILD3: ${BUILD3_}"
+echo "PORT: ${PORTA_}"
+echo "STATION: ${STATION_}"
 
-echo "BUILD1: ${BUILD1} BUILD2: ${BUILD2} BUILD3: ${BUILD3}"
-echo "PORT: ${PORTA}"
-echo "STATION: ${STATION}"
+echo ${STATION_} > "./components/eventengine/include/tipostazione.h"
+CMD_="/home/ccattaneo/esp/esp-idf/tools/idf.py"
 
-echo ${STATION} > "./components/eventengine/include/tipostazione.h"
+if [ ${#BUILD2_} -gt 0 ] #app-flash
+then
+    echo "Caso1"
+    echo "${CMD_}" "${PORTA_}" "${BUILD1_}" "${BUILD2_}" "${BUILD3_}"
+    ("${CMD_}" "${PORTA_}" "${BUILD1_}" "${BUILD2_}" "${BUILD3_}")
+    exit 0
+fi
 
-CMD="/home/ccattaneo/esp/esp-idf/tools/idf.py"
-echo "${CMD}" "${PORTA}" "${BUILD1}" "${BUILD2}" "${BUILD3}"
-"${CMD}" "${PORTA}" "${BUILD1}" "${BUILD2}" "${BUILD3}"
+if [ ${#BUILD1_} -gt 5 ] #monitor
+then
+    echo "Caso2"
+    echo "${CMD_}" "${PORTA_}" "${BUILD1_}"
+    ("${CMD_}" "${PORTA_}" "${BUILD1_}")
+    exit 0
+fi
+
+echo "Caso3"
+echo "${CMD_}" "${BUILD1_}"
+("${CMD_}" "${BUILD1_}")
+exit 0
+
+#echo "${CMD_}" "${PORTA_}" "${BUILD1_}" "${BUILD2_}" "${BUILD3_}"
+#("${CMD_}" "${PORTA_}" "${BUILD1_}" "${BUILD2_}" "${BUILD3_}")
+#exit 0
